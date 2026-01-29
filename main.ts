7acfb0ba5a878485622d911b73257d1b3cbe462c
Brayden Feng <braydenfeng888@gmail.com>
2026-01-29 13:48:09 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,27 @@
-let number = randint(1, 1000)
-let guess = game.askForNumber("Guess a number that is selected 1-1000")
+function check (guess: number, answer: number) {
+    if (guess == answer) {
+        return "correct"
+    } else if (guess < answer) {
+        return "more"
+    } else {
+        return "less"
+    }
+}
+let guess = game.askForNumber("What is your guess from 1 - 1000?")
+let answer = randint(0, 1000)
+let result = check(guess, answer)
+if (result == "less") {
+    game.splash("The answer is less!")
+} else if (result == "more") {
+    game.splash("The answer is more!")
+}
+while (result != "correct") {
+    guess = game.askForNumber("What is your guess?")
+    result = check(guess, answer)
+    if (result == "less") {
+        game.splash("The answer is less!")
+    } else if (result == "more") {
+        game.splash("The answer is more!")
+    }
+}
+game.splash("You win!")
