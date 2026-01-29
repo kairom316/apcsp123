d5f6678e236c7322f362f0ffef293849ecf9893c
Brayden Feng <braydenfeng888@gmail.com>
2026-01-29 13:52:16 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ function check (guess: number, answer: number) {
         return "less"
     }
 }
+info.setLife(10)
 let guess = game.askForNumber("What is your guess from 1 - 1000?")
 let answer = randint(0, 1000)
 let result = check(guess, answer)
@@ -25,3 +26,10 @@ while (result != "correct") {
     }
 }
 game.splash("You win!")
+game.onUpdate(function () {
+    if (guess != answer) {
+        info.changeLifeBy(-1)
+    } else {
+        info.changeScoreBy(info.life())
+    }
+})
