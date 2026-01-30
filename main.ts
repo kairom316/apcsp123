885668a0697e070fb0f91ee412429953b02bc251
Brayden Feng <braydenfeng888@gmail.com>
2026-01-30 13:34:23 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,29 +7,27 @@ function check (guess: number, answer: number) {
         return "less"
     }
 }
-info.setLife(10)
-let guess = game.askForNumber("What is your guess from 1 - 1000?")
+let guess = game.askForNumber("What is your guess from 1 - 1000?", 4)
+let tries = 0
 let answer = randint(0, 1000)
 let result = check(guess, answer)
+tries += 1
 if (result == "less") {
-    game.splash("The answer is less!")
+    game.splash("The answer is less! ", "You have used " + tries + " tries")
 } else if (result == "more") {
-    game.splash("The answer is more!")
+    game.splash("The answer is more! ", "You have used " + tries + " tries")
 }
 while (result != "correct") {
     guess = game.askForNumber("What is your guess?")
     result = check(guess, answer)
+    tries += 1
     if (result == "less") {
-        game.splash("The answer is less!")
+        game.splash("The answer is less! ", "You have used " + tries + " tries")
     } else if (result == "more") {
-        game.splash("The answer is more!")
+        game.splash("The answer is more! ", "You have used " + tries + " tries")
+    }
+    if (tries >= 10) {
+        game.setGameOverMessage(false, "GAME OVER!")
     }
 }
 game.splash("You win!")
-game.onUpdate(function () {
-    if (guess != answer) {
-        info.changeLifeBy(-1)
-    } else {
-        info.changeScoreBy(info.life())
-    }
-})
